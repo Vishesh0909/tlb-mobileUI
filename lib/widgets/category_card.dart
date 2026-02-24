3d06d88cae5b421ab6d082f515691dea245af357
Vishesh96 <srivastava.vishesh96@gmail.com>
2026-02-24 06:33:21 +0530

    feat: add category events listing screen, wire category cards, change remote to Relu-Consultancy

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../models/category_model.dart';
+import '../screens/category_events_screen.dart';
 
 class CategoryCard extends StatelessWidget {
   final CategoryModel category;
@@ -9,49 +10,57 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: category.gradientColors,
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => CategoryEventsScreen(category: category),
         ),
-        borderRadius: BorderRadius.circular(16),
       ),
-      child: Stack(
-        children: [
-          // Icon in top-right
-          Positioned(
-            right: 8,
-            top: 8,
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.25),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Icon(
-                category.icon,
-                size: 22,
-                color: Colors.white,
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: category.gradientColors,
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Stack(
+          children: [
+            // Icon in top-right
+            Positioned(
+              right: 8,
+              top: 8,
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.25),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Icon(
+                  category.icon,
+                  size: 22,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-          // Text in bottom-left
-          Positioned(
-            left: 10,
-            bottom: 10,
-            child: Text(
-              category.name,
-              style: GoogleFonts.poppins(
-                fontSize: 12,
-                fontWeight: FontWeight.w600,
-                color: Colors.white,
-                height: 1.2,
+            // Text in bottom-left
+            Positioned(
+              left: 10,
+              bottom: 10,
+              child: Text(
+                category.name,
+                style: GoogleFonts.poppins(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                  height: 1.2,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
